50061be0f4e56a382e2c1106fae2efcb57df8b23
aiithak <andrewskeller@gmaill.com>
2024-01-07 17:28:27 -0300

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+rootProject.name = "kotlin-multiplatform-core"
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+pluginManagement {
+    repositories {
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    @Suppress("UnstableApiUsage")
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    }
+}
+
+include(":composeApp")
+include(":server")
+include(":shared")
